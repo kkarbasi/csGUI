044585841323311d7f52a2c4a616dd34fb124f44
kkarbasi <kkarbasi@berkeley.edu>
2019-02-13 21:19:14 -0500

    added next/previous functionality

--- app_framework.py
+++ app_framework.py
@@ -20,14 +20,12 @@ class MyApp(QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        self.pushButton_isCS.clicked.connect(self.plot_data)
         self.pushButton_browse.clicked.connect(self.save_filename)
+        self.pushButton_isCS.clicked.connect(self.handle_yes)
+        self.pushButton_notCS.clicked.connect(self.handle_no)
+        self.pushButton_next.clicked.connect(self.handle_next)
+        self.pushButton_previous.clicked.connect(self.handle_previous)
 
-    def plot_data(self):
-        x=range(0, 10)
-        y=range(0, 20, 2)
-        self.plotWidget.canvas.ax.plot(x, y, 'r.')
-        self.plotWidget.canvas.draw()
 
     def save_filename(self):
         filename = QtWidgets.QFileDialog.getOpenFileName()[0]
@@ -46,14 +44,52 @@ class MyApp(QMainWindow, design.Ui_MainWindow):
                 sss = pickle.load(input, encoding='latin1')
             except TypeError:
                 sss = pickle.load(input)
-        pre_index = int(np.round(0.002 / sss.dt))
-        post_index = int(np.round(0.008 / sss.dt))
+        pre_index = int(np.round(0.005 / sss.dt))
+        post_index = int(np.round(0.01 / sss.dt))
 
         self.aligned_cs = np.array([sss.voltage[i - pre_index : i + post_index ] 
             for i in sss.cs_indices])
         self.cs_indices = sss.cs_indices
-        self.plotWidget.canvas.ax.plot(self.aligned_cs[0,:])
+        self.curr_index = 0
+        self.t_axis = np.arange(self.aligned_cs.shape[1])*sss.dt
+        self.plotWidget.canvas.ax.plot(self.t_axis, self.aligned_cs[self.curr_index,:])
+        self.plotWidget.canvas.ax.set_xlabel('Time (s)')
         self.plotWidget.canvas.draw()
+        self.open_new_info_file('./out_test')
+    
+    def open_new_info_file(self, filename):
+        '''
+        Opens an output file to save the information about the accepted 
+        or rejected complex spikes.
+        '''
+        #self.output = open(filename, 'w')
+        
+
+    def handle_yes(self):
+        self.output.write('dasdas')
+        self.output.close()
+
+    def handle_no(self):
+        return
+
+    def handle_next(self):
+        if self.curr_index != self.cs_indices.size-1:
+            self.curr_index = self.curr_index + 1
+            self.plotWidget.canvas.ax.cla()
+            self.plotWidget.canvas.ax.plot(self.t_axis, self.aligned_cs[self.curr_index,:])
+            self.plotWidget.canvas.ax.set_xlabel('Time (s)')
+            self.plotWidget.canvas.draw()
+
+
+    def handle_previous(self):
+        if self.curr_index != 0:
+            self.curr_index = self.curr_index - 1
+            self.plotWidget.canvas.ax.cla()
+            self.plotWidget.canvas.ax.plot(self.t_axis, self.aligned_cs[self.curr_index,:])
+            self.plotWidget.canvas.ax.set_xlabel('Time (s)')
+            self.plotWidget.canvas.draw()
+
+
 
 
 
